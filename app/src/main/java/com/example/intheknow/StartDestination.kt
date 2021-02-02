3a250cbdfd5d2692557d837f993f1fe95bda2168
nealbayya <neal.bayya@gmail.com>
2021-02-02 17:58:22 -0500

    created fragments for home and my logger and figured out basic navigation technique

---
+++ app/src/main/java/com/example/intheknow/StartDestination.kt
@@ -0,0 +1,32 @@
+package com.example.intheknow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [StartDestination.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class StartDestination : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start_destination, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val logger_btn : Button = view.findViewById(R.id.my_logger_btn)
+        logger_btn.setOnClickListener {
+            findNavController().navigate(R.id.action_startDestination_to_myLogger)
+        }
+    }
+}
