f1f08ab2e856764b2d5a936db709582625f02543
nealbayya <neal.bayya@gmail.com>
2021-02-02 11:04:27 -0500

    git test 2

--- app/src/main/java/com/example/intheknow/MainActivity.kt
+++ app/src/main/java/com/example/intheknow/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //git test
+        //git test 2
     }
 }
