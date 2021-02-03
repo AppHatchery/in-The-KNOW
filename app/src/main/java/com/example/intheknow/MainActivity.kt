d3ce2e4867063295c8f2517ac4252cffd6bcdc84
nealbayya <neal.bayya@gmail.com>
2021-02-02 19:55:25 -0500

    set up action bar with nav controller and simplified layout

--- app/src/main/java/com/example/intheknow/MainActivity.kt
+++ app/src/main/java/com/example/intheknow/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.intheknow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController : NavController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController : NavController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
