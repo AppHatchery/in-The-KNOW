d79862d11518c8b59834e92575a3c28003f65d17
nealbayya <neal.bayya@gmail.com>
2021-02-03 13:02:54 -0500

    demo

--- app/src/main/java/com/example/intheknow/MyLogger.kt
+++ app/src/main/java/com/example/intheknow/MyLogger.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ToggleButton
 
 /**
  * A simple [Fragment] subclass.
@@ -21,5 +23,11 @@ class MyLogger : Fragment() {
         return inflater.inflate(R.layout.fragment_my_logger, container, false)
     }
 
+    //override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    //    val tgl_btn : ToggleButton = view.findViewById(R.id.toggleButton)
+    //    if (tgl_btn.isChecked() && tgl_btn.background) {
+    //        tgl_btn.setBackgroundResource(R.drawable.);
+    //    }
+    //}
 
 }
